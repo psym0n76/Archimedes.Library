6625747bf6fdc3be69f618e794ff01a73298a7cb
Simon Burton <burt76@outlook.com>
2020-01-07 19:00:16 +0000

    Add project files.

---
+++ Archimedes.Library.Message/Response/ResponseTrade.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Archimedes.Library.Message.Dto;
+
+namespace Archimedes.Library.Message
+{
+    public class ResponseTrade : IResponse
+    {
+        public string Text { get; set; }
+        public string Status { get; set; }
+        public List<TradeDto> Type { get; set; }
+    }
+}
