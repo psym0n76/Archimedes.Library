e6d339f745fe0e51376853d61e4c65eb268edadd
Simon Burton <burt76@outlook.com>
2020-01-19 19:52:49 +0000

    added hangfire helpers

---
+++ Archimedes.Library.Helpers/HangfireHelpers.cs
@@ -0,0 +1,31 @@
+﻿
+using System.Data.SqlClient;
+
+namespace Archimedes.Library.Helpers
+{
+    public interface IHangfireHelpers
+    {
+        string GetHangfireConnectionString(string dbName,string server);
+    }
+
+    public  class HangfireHelpers : IHangfireHelpers
+    {
+        public  string GetHangfireConnectionString(string dbName,string server)
+        {
+            using (var conn = new SqlConnection($"Server={server};Integrated Security=SSPI"))
+            {
+                conn.Open();
+
+                using (var command = new SqlCommand(string.Format(
+                    @"IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = N'{0}') 
+                                    create database [{0}];
+                      ", dbName), conn))
+                {
+                    command.ExecuteNonQuery();
+                }
+            }
+
+            return $"Server={server};Database={dbName};Integrated Security=SSPI;";
+        } 
+    }
+}
