f8811988079fe60cb01e73c08f184d7358c87de5
Simon Burton <burt76@outlook.com>
2020-01-20 19:20:06 +0000

    added domain project

---
+++ Archimedes.Library.Domain/Config.cs
@@ -0,0 +1,13 @@
+﻿namespace Archimedes.Library.Domain
+{
+    public class Config
+    {
+        public string ApplicationName { get; set; }
+        public string AppVersion { get; set; }
+        public string EnvironmentName { get; set; }
+        public string DatabaseServerConnection { get; set; }
+        public string RabbitHutchConnection { get; set; }
+        public string HangfireDatabaseName { get; set; }
+        public string DatabaseServer { get; set; }
+    }
+}
