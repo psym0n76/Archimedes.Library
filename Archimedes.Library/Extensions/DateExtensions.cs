eabce62bf27c1b41368c9e31c5042b57d3b5cbc4
Simon Burton <burt76@outlook.com>
2020-07-03 13:46:46 +0100

    adsded date time extension

---
+++ Archimedes.Library/Extensions/DateExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace Archimedes.Library.Extensions
+{
+    public static class DateExtensions
+    {
+        public static DateTime BrokerDate(this DateTime dte)
+        {
+            const string sDateFormat = "MM.dd.yyyy HH:mm:ss";
+
+            if (!DateTime.TryParseExact(dte.ToString(CultureInfo.InvariantCulture), sDateFormat, CultureInfo.InvariantCulture,
+                DateTimeStyles.AssumeLocal, out var dateTime))
+            {
+                return DateTime.MinValue;
+            }
+
+            return dateTime;
+        }
+
+
+    }
+}
