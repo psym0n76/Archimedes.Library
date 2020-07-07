77c9b2456a38a0911fab4ec6f4101335394ce53b
Simon Burton <burt76@outlook.com>
2020-07-07 20:49:44 +0100

    moved marketdto

---
+++ Archimedes.Library/Message/Dto/MarketDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Archimedes.Library.Types;
+
+namespace Archimedes.Library.Message.Dto
+{
+    public class MarketDto
+    {
+        public string Name { get; set; }
+        public int Interval { get; set; }
+        public GranularityType TimeFrame { get; set; }
+        public bool Active { get; set; }
+        public DateTime MaxDate { get; set; }
+        public DateTime LastUpdated { get; set; }
+        public string TimeFrameInterval => $"{Interval}{TimeFrame.Value}";
+
+        public override string ToString()
+        {
+            return $"\n{nameof(Name)}: {Name} {nameof(TimeFrameInterval)}: {TimeFrameInterval} {nameof(Active)}: {Active} {nameof(MaxDate)}: {MaxDate} {nameof(LastUpdated)}: {LastUpdated}";
+        }
+    }
+}
