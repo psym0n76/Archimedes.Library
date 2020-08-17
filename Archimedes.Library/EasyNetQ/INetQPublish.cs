1bb320511e292e6185e20332b1518939a42d4832
Simon Burton <burt76@outlook.com>
2020-08-17 18:11:11 +0100

    removed easynetq

--- Archimedes.Library/EasyNetQ/INetQPublish.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace Archimedes.Library.EasyNetQ
-{
-    public interface INetQPublish<T> where T : class
-    {
-        Task PublishMessage(T message);
-    }
-}
