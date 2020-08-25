a0efd30eac3089d2ecbc65fff5b2b85633507e51
Simon Burton <burt76@outlook.com>
2020-08-25 09:59:08 +0100

    added marketId

--- Archimedes.Library/Dto/PriceDto.cs
+++ Archimedes.Library/Dto/PriceDto.cs
@@ -12,6 +12,9 @@ namespace Archimedes.Library.Message.Dto
         [JsonProperty(PropertyName = "market")]
         public string  Market { get; set; }
 
+        [JsonProperty(PropertyName = "marketId")]
+        public string  MarketId { get; set; }
+
         /// <summary>
         /// The market
         /// </summary>
@@ -21,61 +24,61 @@ namespace Archimedes.Library.Message.Dto
         /// <summary>
         /// The price BidOpen price.
         /// </summary>
-        [JsonProperty(PropertyName = "bidopen")]
+        [JsonProperty(PropertyName = "bidOpen")]
         public double BidOpen { get; set; }
 
         /// <summary>
         /// The price BidClose price.
         /// </summary>
-        [JsonProperty(PropertyName = "bidclose")]
+        [JsonProperty(PropertyName = "bidClose")]
         public double BidClose { get; set; }
 
         /// <summary>
         /// The price BidHigh price.
         /// </summary>
-        [JsonProperty(PropertyName = "bidhigh")]
+        [JsonProperty(PropertyName = "bidHigh")]
         public double BidHigh { get; set; }
 
         /// <summary>
         /// The price BidLow price.
         /// </summary>
-        [JsonProperty(PropertyName = "bidlow")]
+        [JsonProperty(PropertyName = "bidLow")]
         public double BidLow { get; set; }
 
         /// <summary>
         /// The price AskOpen price.
         /// </summary>
-        [JsonProperty(PropertyName = "askopen")]
+        [JsonProperty(PropertyName = "askOpen")]
         public double AskOpen { get; set; }
 
         /// <summary>
         /// The price AskClose price.
         /// </summary>
-        [JsonProperty(PropertyName = "askclose")]
+        [JsonProperty(PropertyName = "askClose")]
         public double AskClose { get; set; }
 
         /// <summary>
         /// The price BidHigh price.
         /// </summary>
-        [JsonProperty(PropertyName = "askhigh")]
+        [JsonProperty(PropertyName = "askHigh")]
         public double AskHigh { get; set; }
 
         /// <summary>
         /// The price AskLow price.
         /// </summary>
-        [JsonProperty(PropertyName = "asklow")]
+        [JsonProperty(PropertyName = "askLow")]
         public double AskLow { get; set; }
 
         /// <summary>
         /// The price tick quantity value.
         /// </summary>
-        [JsonProperty(PropertyName = "tickqty")]
+        [JsonProperty(PropertyName = "tickQty")]
         public double TickQty { get; set; }
 
         /// <summary>
         /// The price timestamp.
         /// </summary>
-        [JsonProperty(PropertyName = "timestamp")]
+        [JsonProperty(PropertyName = "timeStamp")]
         public DateTime Timestamp { get; set; }
 
         public override string ToString()
