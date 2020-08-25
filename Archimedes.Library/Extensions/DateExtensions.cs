2bdaf8ec5a1d95ac575ffc39ccb3c419af269197
Simon Burton <burt76@outlook.com>
2020-08-25 11:42:09 +0100

    update date extension

--- Archimedes.Library/Extensions/DateExtensions.cs
+++ Archimedes.Library/Extensions/DateExtensions.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Globalization;
 
 namespace Archimedes.Library.Extensions
 {
@@ -9,15 +8,20 @@ namespace Archimedes.Library.Extensions
         {
             const string sDateFormat = "MM.dd.yyyy HH:mm:ss";
 
-            if (!DateTime.TryParseExact(dte.ToString(CultureInfo.InvariantCulture), sDateFormat, CultureInfo.InvariantCulture,
-                DateTimeStyles.AssumeLocal, out var dateTime))
+            //if (!DateTime.TryParseExact(dte.ToString(CultureInfo.InvariantCulture), sDateFormat, CultureInfo.InvariantCulture,
+            //    DateTimeStyles.AssumeLocal, out var dateTime))
+            //{
+            //    return DateTime.MinValue;
+            //}
+
+            if (DateTime.TryParseExact(dte.ToString(), sDateFormat,
+                System.Globalization.CultureInfo.InvariantCulture,
+                System.Globalization.DateTimeStyles.None, out var startDate))
             {
-                return DateTime.MinValue;
+                return startDate;
             }
 
-            return dateTime;
+            return dte;
         }
-
-
     }
 }
