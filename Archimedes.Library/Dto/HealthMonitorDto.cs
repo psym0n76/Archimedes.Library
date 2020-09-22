78654ed21297345382c0e669c5c7682b510e0b26
Simon Burton <burt76@outlook.com>
2020-09-22 17:27:02 +0100

    added json to healthdto

--- Archimedes.Library/Dto/HealthMonitorDto.cs
+++ Archimedes.Library/Dto/HealthMonitorDto.cs
@@ -1,14 +1,26 @@
 ﻿using System;
+using Newtonsoft.Json;
 
 namespace Archimedes.Library.Message.Dto
 {
     public class HealthMonitorDto
     {
+        [JsonProperty(PropertyName = "url")]
         public string Url  { get; set; }
+
+        [JsonProperty(PropertyName = "version")]
         public string Version { get; set; }
+
+        [JsonProperty(PropertyName = "status")]
         public bool Status { get; set; }
+
+        [JsonProperty(PropertyName = "lastActive")]
         public DateTime LastActive { get; set; }
+
+        [JsonProperty(PropertyName = "lastActiveVersion")]
         public string LastActiveVersion { get; set; }
+
+        [JsonProperty(PropertyName = "lastUpdated")]
         public DateTime LastUpdated { get; set; }
 
         public override string ToString()
