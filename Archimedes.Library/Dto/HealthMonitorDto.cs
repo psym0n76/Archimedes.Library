8be602fe7a992eb8cc143f5a116964693cc49ca7
Simon Burton <burt76@outlook.com>
2020-09-22 16:06:18 +0100

    last active health check

---
+++ Archimedes.Library/Dto/HealthMonitorDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Archimedes.Library.Message.Dto
+{
+    public class HealthMonitorDto
+    {
+        public string Url  { get; set; }
+        public string Version { get; set; }
+        public bool Status { get; set; }
+        public DateTime LastActive { get; set; }
+        public DateTime LastActiveVersion { get; set; }
+
+        public override string ToString()
+        {
+            return $"{nameof(Url)}: {Url}" +
+                   $"\n{nameof(Version)}: {Version}" +
+                   $"\n{nameof(LastActiveVersion)}: {LastActiveVersion} : {nameof(LastActive)}: {LastActive}" +
+                   $"\n{nameof(Status)}: {Status}";
+        }
+    }
+}
