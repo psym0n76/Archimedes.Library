c49945c6ad7d933d36d356ee6665b38d46bffb87
Simon Burton <burt76@outlook.com>
2020-10-09 17:07:37 +0100

    added new metricdto

---
+++ Archimedes.Library/Dto/CandleMetricDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Archimedes.Library.Message.Dto
+{
+    public class CandleMetricDto
+    {
+        [JsonProperty(PropertyName = "maxDate")]
+        public DateTime MaxDate { get; set; }
+
+        [JsonProperty(PropertyName = "minDate")]
+        public DateTime MinDate { get; set; }
+
+        [JsonProperty(PropertyName = "quantity")]
+        public int Quantity { get; set; }
+    }
+}
