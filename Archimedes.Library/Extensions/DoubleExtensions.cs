b4396f5fa8e051ae6926af6ed0f0e102f5578281
Simon Burton <burt76@outlook.com>
2020-10-11 17:59:40 +0100

    added decmal extension

---
+++ Archimedes.Library/Extensions/DoubleExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+
+namespace Archimedes.Library.Extensions
+{
+    public static class DoubleExtensions
+    {
+        public static decimal ToDecimal(this double value)
+        {
+            return decimal.Parse(value.ToString(CultureInfo.InvariantCulture));
+        }
+    }
+}
