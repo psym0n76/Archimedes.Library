36bacad64f0598901c8ec491899e6ff9f02ac3a3
Simon Burton <burt76@outlook.com>
2020-10-13 11:27:15 +0100

    update candle message

--- Archimedes.Library/Message/CandleMessage.cs
+++ Archimedes.Library/Message/CandleMessage.cs
@@ -31,7 +31,7 @@ namespace Archimedes.Library.Message
         {
             return $"\n {nameof(CandleMessage)}" +
                    $"\n  {nameof(Market)}: {Market} {nameof(MarketId)}: {MarketId} {nameof(Interval)}: {Interval} {nameof(TimeFrame)}: {TimeFrame}" +
-                   $"\n  {nameof(StartDate)}: {StartDate} {nameof(EndDate)}: {EndDate} Split into {DateRanges.Count} requests";
+                   $"\n  {nameof(StartDate)}: {StartDate} {nameof(EndDate)}: {EndDate}";
         }
     }
 }
