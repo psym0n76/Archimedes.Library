84be46d4039bce7a2411b12429bae8c18d5c2862
Simon Burton <burt76@outlook.com>
2020-10-18 18:22:59 +0100

    added strategy message

---
+++ Archimedes.Library/Message/StrategyMessage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Archimedes.Library.Message
+{
+    public class StrategyMessage
+    {
+        public string Market { get; set; }
+        public string Granularity { get; set; }
+        public int Interval { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
