9ad550e22fd0d32a441f1281586dcab85fb15b6b
Simon Burton <burt76@outlook.com>
2020-10-19 15:37:43 +0100

    change order

--- Archimedes.Library/Dto/PriceLevelDto.cs
+++ Archimedes.Library/Dto/PriceLevelDto.cs
@@ -6,6 +6,30 @@ namespace Archimedes.Library.Message.Dto
     public class PriceLevelDto
     {
 
+        [JsonProperty(PropertyName = "market")]
+        public string Market { get; set; }
+
+        [JsonProperty(PropertyName = "granularity")]
+        public string Granularity { get; set; }
+
+        [JsonProperty(PropertyName = "active")]
+        public string Active { get; set; }
+
+        [JsonProperty(PropertyName = "tradeType")]
+        public string TradeType { get; set; }
+
+        [JsonProperty(PropertyName = "candleType")]
+        public string CandleType { get; set; }
+
+        [JsonProperty(PropertyName = "timeStamp")]
+        public DateTime TimeStamp { get; set; }
+
+        [JsonProperty(PropertyName = "strategy")]
+        public string Strategy { get; set; }
+
+
+
+
         [JsonProperty(PropertyName = "bidPrice")]
         public double BidPrice { get; set; }
 
@@ -18,26 +42,12 @@ namespace Archimedes.Library.Message.Dto
         [JsonProperty(PropertyName = "askPriceRange")]
         public double AskPriceRange { get; set; }
 
-        [JsonProperty(PropertyName = "tradeType")]
-        public string TradeType { get; set; }
 
-        [JsonProperty(PropertyName = "candleType")]
-        public string CandleType { get; set; }
 
-        [JsonProperty(PropertyName = "strategy")]
-        public string Strategy { get; set; }
 
-        [JsonProperty(PropertyName = "active")]
-        public string Active { get; set; }
 
-        [JsonProperty(PropertyName = "timeStamp")]
-        public DateTime TimeStamp { get; set; }
 
-        [JsonProperty(PropertyName = "market")]
-        public string Market { get; set; }
 
-        [JsonProperty(PropertyName = "granularity")]
-        public string Granularity { get; set; }
 
         [JsonProperty(PropertyName = "lastUpdated")]
         public DateTime LastUpdated {get; set;}
