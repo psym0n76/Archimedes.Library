2ec9b1357408c2970e791aa43ea793da7887e03f
Simon Burton <burt76@outlook.com>
2020-10-27 17:36:38 +0000

    candlemetrics

---
+++ Archimedes.Library/Dto/CandleMetrics.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace Archimedes.Library.Message.Dto
+{
+    public class CandleMetrics
+    {
+        [JsonProperty(PropertyName = "maxDate")]
+        public DateTime MaxDate { get; set; }
+
+        [JsonProperty(PropertyName = "minDate")]
+        public DateTime MinDate { get; set; }
+
+        [JsonProperty(PropertyName = "quantity")]
+        public int Quantity { get; set; }
+    }
+}
