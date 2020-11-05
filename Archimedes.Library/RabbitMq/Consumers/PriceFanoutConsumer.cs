e576b00af819ebe545569b6ad7c37c88ae0e8a0d
Simon Burton <burt76@outlook.com>
2020-11-05 22:08:52 +0000

    added priceconsumer for fanout

---
+++ Archimedes.Library/RabbitMq/Consumers/PriceFanoutConsumer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Archimedes.Library.RabbitMq
+{
+    public class PriceFanoutConsumer : IPriceConsumer
+    {
+        public event EventHandler<MessageHandlerEventArgs> HandleMessage;
+
+        private readonly string _host;
+        private readonly int _port;
+        private readonly string _exchange;
+
+        public PriceFanoutConsumer(string host, int port, string exchange)
+        {
+            _host = host;
+            _port = port;
+            _exchange = exchange;
+        }
+
+        public void Subscribe()
+        {
+            RabbitHealthCheck.ValidateConnection(_host, _port);
+
+            var factory = new ConnectionFactory()
+            {
+                HostName = _host, Port = _port,
+                ClientProvidedName = $"{Assembly.GetCallingAssembly().GetName().Name}.{_exchange}"
+            };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            //channel.QueueDeclare(_queueName, true, false, false);
+            channel.ExchangeDeclare(_exchange, ExchangeType.Direct);
+
+            //channel.QueueBind(_queueName, _exchange, _queueName);
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += Consumer_Received;
+
+            channel.BasicConsume("",
+                autoAck: true,
+                consumer: consumer);
+
+            while (true)
+            {
+                Thread.Sleep(5000);
+            }
+        }
+
+        public void Consumer_Received(object sender, BasicDeliverEventArgs e)
+        {
+            var body = e.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            HandleMessage?.Invoke(sender, new MessageHandlerEventArgs() {Message = message});
+        }
+    }
+}
