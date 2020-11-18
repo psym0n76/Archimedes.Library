6162307937db5160c39a410895f14e93f176aeed
Simon Burton <burt76@outlook.com>
2020-11-18 08:39:39 +0000

    added custom logger

---
+++ Archimedes.Library/Logger/CustomLogger.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+
+namespace Archimedes.Library.Message
+{
+    public class CustomLogger
+    {
+        private readonly StringBuilder _logger;
+        private readonly Stopwatch _timer;
+        private readonly string _title;
+        private long _elapsed = 0;
+
+        //todo add a list to collect different logs
+
+        public CustomLogger(string message)
+        {
+            _timer = new Stopwatch();
+            _timer.Start();
+            _title = message;
+            _logger = new StringBuilder();
+        }
+
+
+        public void EndLog()
+        {
+            var dateMessage = $"{DateTime.Now} {_title} Duration: {_timer.ElapsedMilliseconds}ms";
+            //Debug.WriteLine(dateMessage);
+            _logger.Append(dateMessage);
+        }
+
+        public void UpdateLog(string message)
+        {
+            var time = _timer.ElapsedMilliseconds;
+            var dateMessage = $"{DateTime.Now} {_title} {message} Duration: {time - _elapsed}ms Elapsed: {time}ms";
+            _elapsed = time;
+            //Debug.WriteLine(dateMessage);
+            _logger.AppendLine(dateMessage);
+        }
+
+        public string GetLog()
+        {
+            return _logger.ToString();
+        }
+    }
+}
