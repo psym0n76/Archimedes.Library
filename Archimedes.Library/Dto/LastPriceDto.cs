25a787b012f292c6047048e6fce3b58b6dda615f
Simon Burton <burt76@outlook.com>
2020-11-23 21:50:02 +0000

    added lastprice dto

---
+++ Archimedes.Library/Dto/LastPriceDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Archimedes.Library.Message.Dto
+{
+    public class LastPriceDto
+    {
+        public decimal Bid { get; set; }
+        public decimal Ask { get; set; }
+        public DateTime LastUpdated { get; set; }
+    }
+}
