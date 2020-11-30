a233a30aeb3de7fbc95a3aae00b968ea846b3550
Simon Burton <burt76@outlook.com>
2020-11-30 21:47:25 +0000

    added candle and price level fanout

---
+++ Archimedes.Library/RabbitMq/Consumers/CandleFanoutConsumer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Archimedes.Library.RabbitMq
+{
+    public class CandleFanoutConsumer : ICandleFanoutConsumer
+    {
+        public event EventHandler<MessageHandlerEventArgs> HandleMessage;
+
+        private readonly string _host;
+        private readonly int _port;
+        private readonly string _exchange;
+
+        public CandleFanoutConsumer(string host, int port, string exchange)
+        {
+            _host = host;
+            _port = port;
+            _exchange = exchange;
+        }
+
+        public void Subscribe()
+        {
+            RabbitHealthCheck.ValidateConnection(_host, _port);
+
+            var factory = new ConnectionFactory()
+            {
+                HostName = _host,
+                Port = _port,
+                ClientProvidedName = $"{Assembly.GetCallingAssembly().GetName().Name}.{_exchange}"
+            };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+
+            var queueName = channel.QueueDeclare(durable: false).QueueName;
+            channel.ExchangeDeclare(_exchange, ExchangeType.Fanout);
+
+            channel.QueueBind(queueName, _exchange, "");
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += Consumer_Received;
+
+            channel.BasicConsume(queueName,
+                autoAck: true,
+                consumer: consumer);
+
+            while (true)
+            {
+                Thread.Sleep(5000);
+            }
+        }
+
+        public void Consumer_Received(object sender, BasicDeliverEventArgs e)
+        {
+            var body = e.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            HandleMessage?.Invoke(sender, new MessageHandlerEventArgs() { Message = message });
+        }
+    }
+}
---
+++ Archimedes.Library/RabbitMq/Consumers/Interfaces/IPriceLevelFanoutConsumer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using RabbitMQ.Client.Events;
+
+namespace Archimedes.Library.RabbitMq
+{
+    public interface IPriceLevelFanoutConsumer
+    {
+        event EventHandler<MessageHandlerEventArgs> HandleMessage;
+        void Subscribe();
+
+        void Consumer_Received(object sender, BasicDeliverEventArgs e);
+    }
+}
