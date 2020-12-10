345187a7f7788cb430b941e41e1a84f541c8c010
Simon Burton <burt76@outlook.com>
2020-12-10 18:41:57 +0000

    logging frmat

--- Archimedes.Library/Logger/Log.cs
+++ Archimedes.Library/Logger/Log.cs
@@ -1,19 +1,27 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Archimedes.Library.Logger
 {
     public class Log
     {
+        private decimal _elapsed;
+
         public DateTime TimeStamp { get; set; }
-        public string  Description { get; set; }
-        public long ElapsedTimeMilliseconds { get; set; }
+        public string Description { get; set; }
+
+        public decimal ElapsedTimeSeconds
+        {
+            get => Math.Round(decimal.Parse(_elapsed.ToString(CultureInfo.InvariantCulture)) / 1000, 4);
+            set => _elapsed = value;
+        }
+
+        public string ElapsedTimeText => $"{ElapsedTimeSeconds} sec(s)";
 
         public override string ToString()
         {
             return
-                $"{TimeStamp} Elapsed: {ElapsedTimeMilliseconds.ToString().PadRight(10, ' ')} {nameof(Description)}: {Description}";
-            //return
-            //    $"{nameof(TimeStamp)}:{TimeStamp} {nameof(Description)}:{Description} {nameof(ElapsedTimeMilliseconds)}:{ElapsedTimeMilliseconds}";
+                $"{TimeStamp} Elapsed: {ElapsedTimeText.PadRight(10, ' ')} {nameof(Description)}: {Description}";
         }
     }
 }
