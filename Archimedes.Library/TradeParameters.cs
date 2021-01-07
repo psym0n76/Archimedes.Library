9718421fc7578c67fa5f515568a6b45aed98a3a8
Simon Burton <burt76@outlook.com>
2021-01-07 17:59:36 +0000

    transaction

---
+++ Archimedes.Library/TradeParameters.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using Archimedes.Library.Message.Dto;
+
+namespace Archimedes.Library
+{
+    public class TradeParameters
+    {
+        public decimal EntryPrice { get; set; }
+        public string BuySell { get; set; }
+
+
+        public int SpreadAsPips { get; set; }
+        public decimal Spread => Convert.ToDecimal(SpreadAsPips) / 10000;
+
+
+        public int RiskReward { get; set; }
+        public int TradeCounter { get; set; }
+
+        public string Market { get; set; }
+
+
+        public decimal TargetPrice(int i)
+        {
+            return EntryPrice + (BuySell == "SELL" ? -Spread * i : Spread * i);
+        }
+
+        public decimal SetStopPrice()
+        {
+            return EntryPrice - (BuySell == "SELL" ? -Spread : Spread);
+        }
+
+        public List<TransactionPriceTargetDto> ProfitTargets => SetPriceTargets();
+        public List<TransactionPriceTargetDto> StopTargets => SetStopTargets();
+        public string RiskRewardProfile { get; set; }
+
+        private List<TransactionPriceTargetDto> SetStopTargets()
+        {
+            var stopTargets = new List<TransactionPriceTargetDto>
+            {
+                new TransactionPriceTargetDto()
+                {
+                    EntryPrice = EntryPrice,
+                    Closed = false,
+                    TargetPrice = SetStopPrice(),
+                    LastUpdated = DateTime.Now
+                }
+            };
+
+            return stopTargets;
+        }
+
+        private List<TransactionPriceTargetDto> SetPriceTargets()
+        {
+            var priceTargets = new List<TransactionPriceTargetDto>();
+
+            for (var tradeIndex = 1; tradeIndex <= TradeCounter; tradeIndex++)
+            {
+                priceTargets.Add(new TransactionPriceTargetDto()
+                {
+                    EntryPrice = EntryPrice,
+                    Closed = false,
+                    TargetPrice = TargetPrice(tradeIndex),
+                    LastUpdated = DateTime.Now
+                });
+            }
+
+            return priceTargets;
+        }
+    }
+}
---
+++ Archimedes.Library/Transaction.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Archimedes.Library.Message.Dto;
+
+
+namespace Archimedes.Library
+{
+    public class Transaction
+    {
+        private readonly TradeParameters _tradeParameters;
+
+        public Transaction(TradeParameters tradeParameters)
+        {
+            _tradeParameters = tradeParameters;
+        }
+
+
+        public List<TransactionPriceTargetDto> ProfitTargets => _tradeParameters.ProfitTargets;
+        public List<TransactionPriceTargetDto> StopTargets => _tradeParameters.StopTargets;
+
+
+        public string RiskRewardProfile => _tradeParameters.RiskRewardProfile;
+        public string Market => _tradeParameters.Market;
+        public string BuySell => _tradeParameters.BuySell;
+        public double RiskReward => _tradeParameters.RiskReward;
+        public decimal Spread => _tradeParameters.SpreadAsPips;
+        public decimal EntryPrice => _tradeParameters.EntryPrice;
+        public int TradeCounter => _tradeParameters.TradeCounter;
+        public bool Closed { get; set; }
+
+        public decimal MaxPrice { get; private set; }
+
+
+
+        public void SetMaxPrice(decimal price)
+        {
+            if (BuySell == "buy")
+            {
+                MaxPrice = price > MaxPrice ? price : MaxPrice;
+            }
+            else
+            {
+                if (MaxPrice == 0)
+                {
+                    MaxPrice = price;
+                }
+                else
+                {
+                    MaxPrice = price < MaxPrice ? price : MaxPrice;
+                }
+
+            }
+        }
+
+        public void UpdateTrades(PriceDto price)
+        {
+            foreach (var profitTarget in ProfitTargets)
+            {
+                profitTarget.UpdateTrade(price);
+            }
+
+            Closed = ProfitTargets.Any(a => a.Closed);
+
+            foreach (var stop in StopTargets)
+            {
+                stop.UpdateTrade(price);
+                Closed = stop.Closed;
+            }
+        }
+
+    }
+}
