60ba090bf81c6a607881f6d78f7c28581183c068
Simon Burton <burt76@outlook.com>
2021-01-07 17:39:18 +0000

    added transaction args

---
+++ Archimedes.Library/Dto/TransactionPriceTargetDto.cs
@@ -0,0 +1,27 @@
+﻿
+
+using System;
+
+namespace Archimedes.Library.Message.Dto
+{
+    public class TransactionPriceTargetDto
+    {
+        public decimal EntryPrice { get; set; }
+        public decimal TargetPrice { get; set; }
+        public bool Closed { get; set; }
+        public int Units { get; set; }
+        public DateTime LastUpdated { get; set; }
+
+        public void UpdateTrade(PriceDto price)
+        {
+            if (TargetPrice > EntryPrice)
+            {
+                Closed = price.Bid > TargetPrice;
+            }
+            else
+            {
+                Closed = price.Ask < TargetPrice;
+            }
+        }
+    }
+}
