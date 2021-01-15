58e515a3975fb247943b2edbf40458e128b81f6e
Simon Burton <burt76@outlook.com>
2021-01-15 17:38:41 +0000

    added ttl to producer

--- Archimedes.Library/RabbitMq/IProducer.cs
+++ Archimedes.Library/RabbitMq/IProducer.cs
@@ -3,5 +3,6 @@
     public interface IProducer<T> where T : class
     {
         void PublishMessage(T message, string queueName);
+        void PublishMessage(T message, string queueName, string ttl);
     }
 }
--- Archimedes.Library/RabbitMq/Producer.cs
+++ Archimedes.Library/RabbitMq/Producer.cs
@@ -43,5 +43,35 @@ namespace Archimedes.Library.RabbitMq
                 basicProperties: null,
                 body: body);
         }
+
+        public void PublishMessage(T message, string queueName, string ttl)
+        {
+            RabbitHealthCheck.ValidateConnection(_host, _port);
+
+            var factory = new ConnectionFactory()
+            {
+                HostName = _host,
+                Port = _port,
+                ClientProvidedName = $"{Assembly.GetCallingAssembly().GetName().Name}.{_exchange}.{queueName}"
+            };
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+
+            channel.QueueDeclare(queueName, true, false, true);
+            channel.ExchangeDeclare(exchange: _exchange, type: ExchangeType.Direct);
+            channel.QueueBind(queueName, _exchange, queueName);
+
+            var props = channel.CreateBasicProperties();
+            props.Expiration = ttl;
+
+            var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+
+            channel.BasicPublish(exchange: _exchange,
+                routingKey: queueName,
+                basicProperties: props,
+                body: body);
+        }
     }
 }
